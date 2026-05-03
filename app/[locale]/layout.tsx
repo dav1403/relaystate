4b9b1ac2cee3c9da32ebdd8791792c2b4021cfdf
dav1403 <davidlayani1@gmail.com>
2026-05-03 11:38:10 +0300

    Add Google Analytics G-CVXHR3Q09P

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,10 +2,13 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getTranslations } from "next-intl/server";
+import Script from "next/script";
 import { routing } from "@/i18n/routing";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "../globals.css";
+
+const GA_ID = "G-CVXHR3Q09P";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
 
@@ -51,6 +54,15 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} className={geist.variable}>
+      <head>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+        <Script id="ga-init" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}</Script>
+      </head>
       <body className="min-h-screen flex flex-col bg-[#F8F6F0]">
         <NextIntlClientProvider messages={messages}>
           <Navbar locale={locale} t={navT as Parameters<typeof Navbar>[0]["t"]} />
